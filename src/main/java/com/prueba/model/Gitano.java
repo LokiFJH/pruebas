5497a19b2b7c8a2b982b784a2e4ee1a161b61c22
evaav <evaavilesalvarez@gmail.com>
2024-03-01 22:18:21 +0100

    Fixed dependency bugs

--- src/main/java/com/prueba/model/Gitano.java
+++ src/main/java/com/prueba/model/Gitano.java
@@ -2,16 +2,25 @@ package com.prueba.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Table;
+
 @Data
+@Table(name="gitano")
 public class Gitano {
 
+    @Column(name = "nombre")
     String nombre = null;
 
+    @Column(name = "apellido")
     String appelido = null;
 
+    @Column(name = "edad")
     int edad = 0;
 
+    @Column(name = "hijos")
     int hijos = 0;
 
+    @Column(name = "delitos")
     int delitos = 0;
 }
